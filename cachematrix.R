ea5c972f08754b05a8c6b135195408dc47d9315d
mrguodong <mr.guodong@gmail.com>
2014-10-24 01:02:23 +0900

    Add Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## **********************************************************************
+## *   R Programming Assignment2 
+## *   version 1.0
+## *   Last Update 10/24/2014
+## *   This function is able to cache potentially time-consuming computations. 
+## *   Matrix inversion is usually a costly computation and their may be some benefit to caching the inverse of a matrix rather than compute it repeatedly 
+## *   (there are also alternatives to matrix inversion that we will not discuss here). 
+## *    This assignment is to write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## **************************************************************************
+## * Function Name: makeCacheMatrix
+## This function creates a special "matrix" object that can cache its inverse.
+## **************************************************************************
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -17,8 +26,12 @@ makeCacheMatrix <- function(x = matrix()) {
              getMatrix = getMatrix)	
 }
 
-
-## Write a short comment describing this function
+## **************************************************************************
+## * Function Name: cacheSolve
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
+## **************************************************************************
 
 cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
